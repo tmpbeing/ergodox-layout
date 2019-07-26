8ce2c1c0198e5157c78c8d1b055e8fa5bd084c0e
treia <marc@planelles.com>
2019-07-26 18:15:25 +0200

    Updated base layer and fn layer

--- keymap.c
+++ keymap.c
@@ -33,10 +33,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      ~ |   Q  |   W  |   E  |   R  |   T  | { (  |           | ) }  |   Y  |   U  |   I  |   O  |   P  |   \    |
      * |--------+------+------+------+------+------|   [  |           | ]    |------+------+------+------+------+--------|
      * | Tab    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |'       |
-     * |--------+------+------+------+------+------|Tmux  |           | Esc  |------+------+------+------+------+--------|
-     * | LShift |Z\Ctrl|   X  |   C  |   V  |   B  | WM   |           |      |   N  |   M  |   ,  |   .  |//Ctrl| Fn     |
+     * |--------+------+------+------+------+------|Tmux  |           | Tmux |------+------+------+------+------+--------|
+     * | LShift |Z\Ctrl|   X  |   C  |   V  |   B  | WM   |           |  WM  |   N  |   M  |   ,  |   .  |//Ctrl| Fn     |
      * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-     *   |      |      |      |      |      |                                       |      |      |      |      |      |
+     *   |      |      |      |      | Esc  |                                       | Esc  |      |      |      |      |
      *   `----------------------------------'                                       `----------------------------------'
      *                                        ,-------------.       ,-------------.
      *                                        | LAlt | LGui |       | FR   | Del  |
@@ -53,19 +53,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_DELT,         KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TD(CT_LBP),
   KC_TAB,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,
   KC_LSFT,        LCTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    TD(CT_WM_TMUX),
-  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_ESC,
                                                            KC_LALT, KC_LGUI,
                                                                           KC_CAPSLOCK,
                                                          KC_BSPC, KC_LSFT, MO(NUM),
   // right hand
-  TT(FN),     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS,
-  TD(CT_RBP), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_BSLS,
-              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_QUOT,
-  KC_ESC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), TT(FN),
-                       KC_NO,   KC_NO,   KC_NO,   KC_NO,           KC_NO,
-  TG(FR),     KC_DELT,
+  TT(FN),         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS,
+  TD(CT_RBP),     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_BSLS,
+                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_QUOT,
+  TD(CT_WM_TMUX), KC_N,    KC_M,    KC_COMM, KC_DOT,  RCTL_T(KC_SLSH), TT(FN),
+                           KC_ESC,  KC_NO,   KC_NO,   KC_NO,           KC_NO,
+  TG(FR),         KC_DELT,
   KC_LEAD,
-  TG(NUM),    KC_ENT, KC_SPC
+  TG(NUM),        KC_ENT,  KC_SPC
 ),
 
 /* Keymap 1: WM controls
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // right hand
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
+           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,
@@ -156,43 +156,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 3: FN : Media, mouse, arrows and function keys
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      | MUP  |      |      |      |           |      | HOME | PGUP | PGDW | END  |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
+ * |        |      | MLEFT| MDOWN|MRIGHT|      |------|           |------| LEFT | DOWN |  UP  | RIGHT|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   |      |      |      |LCLICK|RCLICK|                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
+ *                                        |      |      |       | MUTE |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
+ *                                 |      | PLAY | PREV |       | VOL+ |      |      |
+ *                                 | STOP | PAUSE|------|       |------|      |      |
+ *                                 |      |      | NEXT |       | VOL- |      |      |
  *                                 `--------------------'       `--------------------'
  */
 [FN] = LAYOUT_ergodox(
   // left hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                               KC_TRNS, KC_TRNS,
-                                                        KC_TRNS,
-                                      KC_TRNS, KC_TRNS, KC_TRNS,
+                                               KC_TRNS,       KC_TRNS,
+                                                              KC_MEDIA_PREV_TRACK,
+                                               KC_MEDIA_STOP, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK,
   // right hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS,
-  KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS
+  KC_TRNS, KC_F6,   KC_F7,     KC_F8,   KC_F9,    KC_F10,  KC_TRNS,
+  KC_TRNS, KC_HOME, KC_PGDOWN, KC_PGUP, KC_END,   KC_TRNS, KC_TRNS,
+           KC_LEFT, KC_DOWN,   KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+                    KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+  KC_AUDIO_MUTE, KC_TRNS,
+  KC_AUDIO_VOL_UP,
+  KC_AUDIO_VOL_DOWN, KC_TRNS, KC_TRNS
 ),
 
 /* Keymap 4: Special french characters
@@ -229,7 +229,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // right hand
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
+           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,
@@ -272,7 +272,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // right hand
   KC_TRNS, KC_TRNS, KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_PLUS, KC_TRNS,
-           KC_TRNS, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_PLUS, KC_MPLY,
+           KC_TRNS, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_PLUS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_ENTER, KC_TRNS,
                     KC_KP_0, KC_KP_0, KC_KP_DOT, KC_KP_ENTER, KC_TRNS,
   KC_TRNS, KC_TRNS,
